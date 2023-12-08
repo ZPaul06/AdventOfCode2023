fdf52df9d585fe6f4e2af439c3eca630e3fa30fb
ZPaul06 <paul_zwick@yahoo.com>
2023-12-08 09:33:12 +0100

    Added solution for part 2 of day 7.

--- src/day7.rs
+++ src/day7.rs
@@ -4,48 +4,64 @@ use std::fs::File;
 use std::io::Read;
 use std::iter::zip;
 
-fn get_type(card: &str, part_two: bool) -> usize {
-    let mut chars = card.chars().collect::<Vec<_>>();
+fn get_type(hand: &str, part_two: bool) -> usize {
+    let mut chars = hand.chars().collect::<Vec<_>>();
     chars.sort();
     chars.dedup();
+    let j_s = hand.chars().filter(|c| *c == "J".chars().next().unwrap()).collect::<Vec<_>>().len();
 
-    if chars.len() == 2 {
-        let mut c = card.chars().collect::<Vec<_>>();
+    if chars.len() == 1 {
+        return 6
+    } else if chars.len() == 2 {
+        if part_two && j_s != 0 {
+            return 6
+        }
+        let mut c = hand.chars().collect::<Vec<_>>();
         c.sort();
         if c[0] == c[1] && c[3] == c[4]  {
-            4
+            return 4
         } else {
-            5
+            return 5
         }
     } else if chars.len() == 3 {
-        let mut c = card.chars().collect::<Vec<_>>();
+        let mut c = hand.chars().collect::<Vec<_>>();
         c.sort();
         if c[0] == c[2] || c[1] == c[3] || c[2] == c[4] {
-            3
+            if part_two && j_s != 0 {
+                return 5
+            }
+            return 3
         } else {
-            2
+            if part_two && j_s == 1 {
+                return 4
+            } else if part_two && j_s == 2 {
+                return 5
+            }
+            return 2
         }
-    } else if chars.len() == 1 {
-        6
+    } else if chars.len() == 4 {
+        if part_two && j_s != 0 {
+            return 3
+        }
+        return 1
     } else {
-        6 - chars.len() - 1
+        if part_two && j_s != 0 {
+            return 1
+        }
+        return 0
     }
 }
 
 fn compare_hands(hand_a: &str, hand_b: &str, map: &HashMap<&str, usize>, part_two: bool) -> Ordering {
     let (type_a, type_b) = (&get_type(hand_a, part_two), &get_type(hand_b, part_two));
-    if !(part_two && (hand_a.contains("J") || hand_b.contains("J"))) {
-        if type_a != type_b {
-            type_a.partial_cmp(type_b).unwrap()
-        } else {
-            for (card_a, card_b) in zip(hand_a.chars(), hand_b.chars()) {
-                if card_a != card_b {
-                    return map.get(card_a.to_string().as_str()).unwrap().partial_cmp(map.get(card_b.to_string().as_str()).unwrap()).unwrap()
-                }
-            }
-            Ordering::Equal
-        }
+    if type_a != type_b {
+        type_a.partial_cmp(type_b).unwrap()
     } else {
+        for (card_a, card_b) in zip(hand_a.chars(), hand_b.chars()) {
+            if card_a != card_b {
+                return map.get(card_a.to_string().as_str()).unwrap().partial_cmp(map.get(card_b.to_string().as_str()).unwrap()).unwrap()
+            }
+        }
         Ordering::Equal
     }
 }
@@ -58,17 +74,20 @@ pub fn run() {
     let map = HashMap::from([("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("T", 10), ("J", 11), ("Q", 12), ("K", 13), ("A", 14)]);
     let map_2 = HashMap::from([("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("T", 10), ("J", 1), ("Q", 12), ("K", 13), ("A", 14)]);
 
-    let mut sum = 0;
+    let (mut sum, mut sum2) = (0, 0);
 
     let mut hands_and_bids = input.split("\n").collect::<Vec<_>>().iter().map(|s| s.split_whitespace().collect::<Vec<_>>()).collect::<Vec<_>>();
     let mut hands_and_bids_2 = hands_and_bids.clone();
 
     hands_and_bids.sort_by(|h_b_a, h_b_b| compare_hands(h_b_a[0], h_b_b[0], &map, false));
-    //hands_and_bids_2.sort_by(|h_b_a, h_b_b| compare_hands(h_b_a[0], h_b_b[0], &map_2, true));
+    hands_and_bids_2.sort_by(|h_b_a, h_b_b| compare_hands(h_b_a[0], h_b_b[0], &map_2, true));
 
     for (fact, h_b) in hands_and_bids.iter().enumerate() {
         sum += (fact + 1) * h_b[1].parse::<usize>().unwrap();
     }
+    for (fact, h_b) in hands_and_bids_2.iter().enumerate() {
+        sum2 += (fact + 1) * h_b[1].parse::<usize>().unwrap();
+    }
 
-    println!("--7-- Part One: {}", sum);
+    println!("--7-- Part One: {}\n--7-- Part Two: {}", sum, sum2);
 }
